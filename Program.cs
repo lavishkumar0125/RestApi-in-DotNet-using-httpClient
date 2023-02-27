f5d745c27b90d6ef9101cdc1d62fcb7918e7b1fa
LAVISH KUMAR <44172938+lavishkumar0125@users.noreply.github.com>
2023-02-27 14:18:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+
+namespace MyRestApi 
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+           await Program.getSingleUser(sharedClient);
+
+        }
+        // adding shared base address for all api requests
+        private static HttpClient sharedClient = new()
+        {
+            BaseAddress = new Uri("https://reqres.in/api/"),
+        };
+
+        // get request to get Single User 
+        static async Task getSingleUser(HttpClient httpClient)
+        {
+            int u_id = 2    ;
+            String url = $"users/{u_id}";
+            using HttpResponseMessage response = await httpClient.GetAsync(url);
+            Console.WriteLine(response.StatusCode);
+
+            if(response.IsSuccessStatusCode) { 
+            JsonElement root = JsonDocument.Parse(await response.Content.ReadAsStringAsync()).RootElement;
+            
+            var user = root.GetProperty("data");
+            var Id = user.GetProperty("id");
+            var firstName = user.GetProperty("first_name");
+            var lastName = user.GetProperty("last_name");
+            var email = user.GetProperty("email");
+            var avatar = user.GetProperty("avatar");
+
+            Console.WriteLine($"Id: {Id}\nName: {firstName} {lastName}\nEmail:{email}");
+            Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("User Not Found: Enter Valid User");
+            }
+        }
+
+    }
+}
